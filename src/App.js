a6204a3196a2965d25cacbbc6c81c479eb6ec34a
tornordqvist <tor.nordqvist@captor.se>
2018-09-15 18:08:18 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,21 +4,22 @@ import PropTypes from "prop-types";
 import './App.css';
 
 export class App extends Component {
-  render() {
-    var name = "Boilerplate";
-    return (
-      <div className="App">
-			<div className="App-header">
-				<h2>{ name }</h2>
-			</div>
-			<div className="row">
-				<div className="col-12">
-					Hello
+	render() {
+		console.log(lorumipsum);
+		var name = "Boilerplate";
+		return (
+			<div className="App">
+				<div className="App-header">
+					<h2>{ name }</h2>
+				</div>
+				<div className="row">
+					<div className="col-12">
+						Hello
+					</div>
 				</div>
 			</div>
-		</div>
-    );
-  }
+		);
+	}
 }
 
 export default App;
